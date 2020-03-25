4b40e882e885f54ac12c9a7ca6ef1d2162cf5de1
yznt <nsso@foxmail.com>
2020-03-25 14:48:00 +1300

    fix deps

--- packages/laminar-indexer/index.ts
+++ packages/laminar-indexer/index.ts
@@ -5,8 +5,8 @@ import { options } from '@laminar/api';
 dotenv.config();
 
 const run = async (): Promise<void> => {
-  const dbUrl = process.env.LAMINAR_DB_URI as string;
-  const wsUrl = process.env.LAMINAR_WS_URL || 'wss://testnet-node-1.laminar-chain.laminar.one/ws';
+  const dbUrl = process.env.DB_URI as string;
+  const wsUrl = process.env.WS_URL || 'wss://testnet-node-1.laminar-chain.laminar.one/ws';
   const indexer = await Indexer.create({ dbUrl, wsUrl, types: options({}).types as any, sync: true });
   await indexer.start();
 };
