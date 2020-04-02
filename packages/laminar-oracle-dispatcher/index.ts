b5cf9977a103a79abf0b6e2f1dc151f90487065a
Xiliang Chen <xlchen1291@gmail.com>
2020-04-02 20:08:13 +1300

    add laminar oracle dispatcher (#10)

---
+++ packages/laminar-oracle-dispatcher/index.ts
@@ -0,0 +1,109 @@
+import dotenv from 'dotenv';
+import { options } from '@laminar/api';
+import { builder, onInterval, createEvent, onEvent } from '@orml/dispatcher';
+import { ApiManager } from '@orml/api';
+import { toBaseUnit, defaultLogger } from '@orml/util';
+import { AlphaVantage } from '@orml/fetcher';
+import { configureLogger } from '@orml/app-util';
+
+const logger = defaultLogger.createLogger('app');
+
+const readEnvConfig = (overrideConfig: object) => {
+  dotenv.config();
+  const config = {
+    wsUrl: process.env.WS_URL as string,
+    seed: process.env.SEED as string,
+    alphaVantageApiKey: process.env.ALPHA_VANTAGE_API_KEY as string,
+    slackWebhook: process.env.SLACK_WEBHOOK,
+    interval: Number(process.env.INTERVAL || 1000 * 60 * 5), // default to 5 mins
+    env: process.env.NODE_ENV || 'development',
+    logFilter: process.env.LOG_FILTER,
+    logLevel: process.env.LOG_LEVEL,
+    ...overrideConfig
+  };
+
+  if (!config.wsUrl) {
+    throw new Error('Missing WS_URL');
+  }
+  if (!config.seed) {
+    throw new Error('Missing SEED');
+  }
+  if (!config.alphaVantageApiKey) {
+    throw new Error('Missing ALPHA_VANTAGE_API_KEY');
+  }
+  return config;
+};
+
+const CURRENCIES = {
+  EUR: 'FEUR',
+  JPY: 'FJPY',
+  BTC: 'FBTC',
+  ETH: 'FETH'
+};
+
+const SYMBOLS: [keyof typeof CURRENCIES, string][] = [
+  ['BTC', 'USD'],
+  ['ETH', 'USD'],
+  ['EUR', 'USD'],
+  ['JPY', 'USD']
+];
+
+const run = async (overrideConfig: Partial<ReturnType<typeof readEnvConfig>> = {}) => {
+  const config = readEnvConfig(overrideConfig);
+  configureLogger({
+    slackWebhook: config.slackWebhook,
+    production: config.env === 'production',
+    filter: config.logFilter,
+    level: config.logLevel
+  });
+
+  logger.info('Starting...');
+
+  const api = await ApiManager.create({
+    ...options({}),
+    wsEndpoint: config.wsUrl,
+    account: config.seed
+  });
+
+  const alphaVantage = new AlphaVantage(config.alphaVantageApiKey);
+
+  const onPrice = createEvent<Array<{ currency: string; price: string }>>('onPrice');
+
+  const readData = async () => {
+    return alphaVantage
+      .getAll(SYMBOLS)
+      .then((result) => {
+        const prices = result.map((x, idx) => ({ currency: CURRENCIES[SYMBOLS[idx][0]], price: x }));
+        prices.push({ currency: 'AUSD', price: '1' });
+        onPrice.emit(prices);
+
+        logger.log('readData', prices);
+      })
+      .catch((error) => {
+        logger.info('AlphaVantage read data error', error);
+      });
+  };
+
+  const feedData = async (data: Array<{ currency: string; price: string }>) => {
+    const tx = api.api.tx.oracle.feedValues(data.map(({ currency, price }) => [currency, toBaseUnit(price).toFixed()]));
+    const result = api.signAndSend(tx);
+    await result.send;
+    const res = await api.signAndSend(tx).inBlock;
+
+    logger.info('feedData done', { blockHash: res.blockHash, txHash: res.txHash });
+  };
+
+  builder()
+    .addHandler(onInterval({ interval: config.interval, immediately: true }, readData))
+    .addHandler(onEvent(onPrice, feedData))
+    .build();
+
+  logger.info('Ready');
+};
+
+export default run;
+
+// if called directly
+if (require.main === module) {
+  run();
+}
