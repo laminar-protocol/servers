faf22cb6aee63355bd6d73927f69a6f6991ec7f8
aniiantt <nsso@foxmail.com>
2020-09-01 18:48:28 +1200

    fix default port

--- packages/laminar-indexer/index.ts
+++ packages/laminar-indexer/index.ts
@@ -3,14 +3,14 @@ import { options } from '@laminar/api';
 import axios from 'axios';
 
 const run = async (): Promise<void> => {
-  const dbUrl = (process.env.DB_URI as string) || 'postgres://postgres:postgres@localhost:5433/postgres';
+  const dbUrl = (process.env.DB_URI as string) || 'postgres://postgres:postgres@localhost:5432/postgres';
   const wsUrl = (process.env.WS_URL as string) || 'wss://testnet-node-1.laminar-chain.laminar.one/ws';
   const indexer = await Indexer.create({ dbUrl, wsUrl, types: options({}).types as any, sync: true });
   await indexer.start();
 };
 
 const importHaruraMetadata = () => {
-  const url = (process.env.HASURA_URL as string) || 'http://localhost:8081/v1/query';
+  const url = (process.env.HASURA_URL as string) || 'http://localhost:8080/v1/query';
   return axios.post(url, { type: 'replace_metadata', args: require('./hasura_metadata.json') });
 };
 
